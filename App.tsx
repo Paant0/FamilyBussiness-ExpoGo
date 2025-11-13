cb546469696f5378063f4a852355f640c0d44a5a
tatiana} <dt.guatapo@unimayor.edu.co>
2025-11-13 16:42:14 -0500

    Agrego login, Home, FAB y UserContext: módulo completo y funcionando

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { UserProvider, useUser } from "./context/UserContext";
+import Home from "./components/Home";
+import Login from "./components/login";
+
+function MainApp() {
+  const { user } = useUser();
+  return <View style={styles.container}>{user ? <Home /> : <Login />}</View>;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserProvider>
+      <MainApp />
+    </UserProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
