6e1e4788433ff6a2f016ac48901ae2dc4da4b886
ponquew <hervindc15@gmail.com>
2025-11-15 22:46:09 -0500

    arreglar cositas y agg domumentation :#

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { UserProvider, useUser } from "./context/UserContext";
-import Home from "./components/Home";
-import Login from "./components/login";
+import Login from "./src/components/login";
+import Home from "./src/components/Home";
+import HomeScreen from "./src/components/HomeScreen";
 
 function MainApp() {
   const { user } = useUser();
-  return <View style={styles.container}>{user ? <Home /> : <Login />}</View>;
+  return <View style={styles.container}>{user ? <HomeScreen /> : <Login />}</View>;
 }
 
 export default function App() {
